22a7d0e10526aeb542814caa2ba917535a76360c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-06 22:26:01 +0000

    Import configuration and apiKey

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Configuration, OpenAIApi } from 'openai'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [prompt, setPrompt] = useState('');
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  })
+  const openai = new OpenAIApi(configuration);
 
+  const generateImage = async() => {
+    const response = await openai.createImage({
+      prompt: "Say this is a test",
+      n: 1,
+      size: "512x512"
+    })
+  }
   return (
     <div className="">
-      
+
     </div>
   )
 }
