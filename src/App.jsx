40df15a72085779372fe8164a5ae925cd12dcc63
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-07 23:00:02 +0300

    Create input and button

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,24 @@ function App() {
 
   const generateImage = async() => {
     const response = await openai.createImage({
-      prompt: "Say this is a test",
+      prompt: prompt,
       n: 1,
       size: "512x512"
-    })
+    });
+    console.log(response.data.data[0].url);
   }
   return (
-    <div className="">
-
+    <div className="flex items-center justify-center flex-col py-5 bg-black">
+      <h1 className='text-white' >Generate Image</h1>
+      <div className=" py-5">
+        <input
+          className='outline-black border-black rounded px-6'
+          placeholder='Type in words to indicate the image needed'
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+        />
+      </div>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={generateImage}>Generate Image</button>
     </div>
   )
 }
